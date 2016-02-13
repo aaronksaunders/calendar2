2429d519097ee4b5998fc740ab55a1a193f7c98a
aaronksaunders <aaron@clearlyinnovative.com>
2016-02-13 18:45:07 -0500

    initial checkin

---
+++ calendar2/Objc-Bridge-Header.h
@@ -0,0 +1,9 @@
+
+#ifndef SwiftExample_Objc_Bridge_Header_h
+#define SwiftExample_Objc_Bridge_Header_h
+
+#import "FSCalendar.h"
+#import "FSCalendarHeader.h"
+#import "NSString+FSExtension.h"
+
+#endif
--- calendar2/ViewController.swift
+++ calendar2/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
+
+//
+// for help with the layout
+// http://stackoverflow.com/questions/18174980/creating-a-percentage-based-ios-layout
+//
 class ViewController: UIViewController {
+
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var tableView: EventViewController!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = EventViewController()
+        tableView.delegate = EventViewController()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
+
+//
+// Seperate functions to support the calendar
+//
+extension ViewController: FSCalendarDataSource, FSCalendarDelegate  {
+    
+    
+    func calendarCurrentMonthDidChange(calendar: FSCalendar!) {
+        print("\(calendar)")
+    }
+    
+    func calendar(calendar: FSCalendar!, didSelectDate date: NSDate!) {
+        NSLog("calendar did select date \(calendar.stringFromDate(date))")
+    }
+    
+    func calendar(calendar: FSCalendar!, numberOfEventsForDate date: NSDate!) -> Int {
+        let day = calendar.dayOfDate(date)
+        return day % 5 == 0 ? day/5 : 0;
+    }
+    
+    func calendar(calendar: FSCalendar!, hasEventForDate date: NSDate!) -> Bool {
+        <#code#>
+    }
+}
+
 
